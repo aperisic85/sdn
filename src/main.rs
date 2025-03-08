0f90e9750c91d839e954099a5487cfc5cd16af2a
An85_lap <perisic.ante@gmail.com>
2025-03-08 17:02:34 +0100

    add actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, web};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(hello_world)))
+        .bind("127.0.0.1:8081")?
+        .run()
+        .await
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, there!"
 }
